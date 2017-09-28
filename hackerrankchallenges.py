5708577ac21770d45569d177c7a9b9774cb45c1b
matthewharrilal <matthewharrilal@gmail.com>
2017-09-28 00:11:16 -0700

    In the midst of completing a series of hackerrank challenges

---
+++ hackerrankchallenges.py
@@ -0,0 +1,23 @@
+# Hackerrank Challenges 1
+
+def sleep(name):
+    print("%s sleeps for 8 hours" %(name))
+
+sleep("Tigger")
+
+#Hackerrank Challenge Number 2
+favorite_food = "meat"
+
+def eat(name, food):
+    print("%s eats %s "%(name, food))
+
+
+    if favorite_food != "meat":
+        return
+    else:
+        print("YUM! %s wants more %s" %(name, food))
+
+eat("Tigger", "meat")
+
+# Hackerrank Coding Challenges
+
